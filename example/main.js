8987a1ecf6c5ee088fd47d92571b9424f3605786
HaoDaWang <1306036576@qq.com>
2019-10-08 10:05:37 +0800

    [Feat] update config

--- example/main.js
+++ example/main.js
@@ -14,7 +14,7 @@ const writeFile = promisify(FS.writeFile);
       outputFileName,
       Postcss(
         postcssRelaxedUnit({
-          rules: [{ rx: "add(1).sub(2).mul(3).div(9).unit(rem)" }]
+          rules: { rx: "add(1).sub(2).mul(3).div(9).unit(rem)" }
         })
       ).process(cssContent).css
     );
